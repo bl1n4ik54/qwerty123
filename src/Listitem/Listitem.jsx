59abe31c6d53e4998e4a7a73588fc30ac35dda3b
Shavkat <shavkatk06@gmail.com>
2024-10-21 17:06:24 +0300

    6th commit

--- src/Listitem/Listitem.jsx
+++ src/Listitem/Listitem.jsx
@@ -2,24 +2,50 @@ import React, { useState } from 'react';
 import { randomId } from '../random';
 import s from './Listitem.module.css'
 
+
+let arr
+
+if(localStorage.getItem('TODO')){
+	arr = JSON.parse(localStorage.getItem('TODO'))
+} else {
+	arr = []
+}
+
 export const TaskInput = () => {
 	const [task, setTask] = useState('')
-	const [tasks, setTasks] = useState([])
-	const [editing, setEditing] = useState(null)
-
-	const createElemToDo = (e) => {
-		e.preventDefault()
+	const [tasks, setTasks] = useState(arr)
+	
+	let func = (a) => {
+		localStorage.setItem('TODO', JSON.stringify(a))
+	}
+	const createElemToDo = () => {
 		if (task !== "") {
-			setTasks([...tasks, { id: randomId(16), text: task, isEdit: false, isChecked: false }])
+			let copy = [...tasks, { id: randomId(16), text: task, isEdit: false, isChecked: false }]
+			setTasks(copy)
 			setTask('')
+			func(copy)
 		}
 	}
 	const saveTask = (id, text) => {
-		setTasks(tasks.map((task) => task.id == id ? { ...task, text } : task))
-		setEditing(null)
-	}	
+		let copy = tasks.map((task) => task.id == id ? { ...task, text } : task)
+		setTasks(copy)
+		func(copy)
+	}
+
+	const toggler = (id) => {
+		let copy = tasks.map((task) => {
+			if (task.id == id) {
+				task.isEdit = !task.isEdit
+			}
+			return task
+		})
+		setTasks(copy)
+		func(copy)
+	}
 	const deleteTask = (id) => {
-		setTasks(tasks.filter((task) => task.id !== id))
+		let copy = tasks.filter((task) => task.id !== id)
+		setTasks(copy)
+		func(copy)
 	}
 
 	return (
@@ -27,20 +53,20 @@ export const TaskInput = () => {
 			<div>
 				<input
 					value={task}
-					onChange={(e) => setTask(e.target.value)}
+					onChange={e => setTask(e.target.value)}
 					placeholder="Введите задачу"
 				/>
-				<button onClick={createElemToDo}>Создать</button>
+				<button onClick={() => createElemToDo()}>Создать</button>
 			</div>
 			<ul>
-				{tasks.map((task) => (
+				{tasks.map(task => (
 					<>
 						<li key={task.id}>
-							{editing == task.id ? (
+							{task.isEdit == true ? (
 								<input value={task.text}
 									onChange={(e) => saveTask(task.id, e.target.value)} />
 							) : (<span>{task.text}</span>)}
-							<button className={s.iconChange} onClick={() => setEditing(task.id)}>
+							<button className={s.iconChange} onClick={() => toggler(task.id)}>
 								<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round" />
 								</svg>
